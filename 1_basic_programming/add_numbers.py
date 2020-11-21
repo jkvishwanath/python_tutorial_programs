14d19684e4689a3fbe4a9fabbb7386a83ad47cae
jk vishwa <jkashivishwanath5@gmail.com>
2020-11-21 19:00:09 +0530

    adding flaot numbers

--- 1_basic_programming/add_numbers.py
+++ 1_basic_programming/add_numbers.py
@@ -1,5 +1,14 @@
 
+#Program1
 num1 = 23
 num2 = 45
 sum = num1+num2
 print("sum of {0} + {1} is {2}".format(num1,num2,sum))
+
+
+#Program2
+num3=input("enter 1st number")
+num4=input("ente 2nd number")
+sum2=float(num3)+float(num4)
+print("{0}".format(sum2))
+
