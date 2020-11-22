058bd6077498e1d6b77b2d18d4743977798a6082
jk vishwa <jkashivishwanath5@gmail.com>
2020-11-22 23:05:50 +0530

    program to whether number is prime or not

---
+++ 1_basic_programming/check_prime_number.py
@@ -0,0 +1,12 @@
+
+
+num = int(input("enter the number\n"))
+
+for i in range(2,num):
+    if num == 1 or num == 1:
+        print(1)
+    if(num % i == 0 ):
+        print("{0} is not a prime num")
+        break
+else:
+    print("{0} is a prime num")
