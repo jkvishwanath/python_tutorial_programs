4fa027f9148929ba1e3902257a60c90abf784013
jk vishwa <jkashivishwanath5@gmail.com>
2020-12-06 01:02:26 +0530

    adding some more programs on logging

---
+++ logging_module_examples/3.logging_using_dict_config_examples/1.Sample_logging.py
@@ -0,0 +1,35 @@
+import logging
+import logging.config
+
+logger = logging.getLogger(__name__)
+
+# load config from file
+# logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
+# or, for dictConfig
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,  # this fixes the problem
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True
+        }
+    }
+})
+
+logger.info('It works!')
+logging.info("It works")
+
+# ref : https://fangpenlin.com/posts/2012/08/26/good-logging-practice-in-python/
